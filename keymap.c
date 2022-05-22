e3d9209de6e33fe6bcae9900cf72383ac264bbd4
Luc Ritchie <luc.ritchie@gmail.com>
2022-05-22 12:00:00 +0000

    Initial commit

---
+++ keymap.c
@@ -0,0 +1,149 @@
+#include QMK_KEYBOARD_H
+#include "keymap_belgian.h"
+#include "keymap_bepo.h"
+#include "keymap_br_abnt2.h"
+#include "keymap_canadian_multilingual.h"
+#include "keymap_contributions.h"
+#include "keymap_croatian.h"
+#include "keymap_czech.h"
+#include "keymap_danish.h"
+#include "keymap_estonian.h"
+#include "keymap_french.h"
+#include "keymap_german.h"
+#include "keymap_german_ch.h"
+#include "keymap_hungarian.h"
+#include "keymap_italian.h"
+#include "keymap_jp.h"
+#include "keymap_korean.h"
+#include "keymap_lithuanian_azerty.h"
+#include "keymap_nordic.h"
+#include "keymap_norwegian.h"
+#include "keymap_portuguese.h"
+#include "keymap_romanian.h"
+#include "keymap_russian.h"
+#include "keymap_slovak.h"
+#include "keymap_slovenian.h"
+#include "keymap_spanish.h"
+#include "keymap_swedish.h"
+#include "keymap_turkish_q.h"
+#include "keymap_uk.h"
+#include "keymap_us_international.h"
+#include "version.h"
+
+#define KC_MAC_UNDO LGUI(KC_Z)
+#define KC_MAC_CUT LGUI(KC_X)
+#define KC_MAC_COPY LGUI(KC_C)
+#define KC_MAC_PASTE LGUI(KC_V)
+#define KC_PC_UNDO LCTL(KC_Z)
+#define KC_PC_CUT LCTL(KC_X)
+#define KC_PC_COPY LCTL(KC_C)
+#define KC_PC_PASTE LCTL(KC_V)
+#define ES_LESS_MAC KC_GRAVE
+#define ES_GRTR_MAC LSFT(KC_GRAVE)
+#define ES_BSLS_MAC ALGR(KC_6)
+#define NO_PIPE_ALT KC_GRAVE
+#define NO_BSLS_ALT KC_EQUAL
+#define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
+#define BP_NDSH_MAC ALGR(KC_8)
+#define SE_SECT_MAC ALGR(KC_6)
+#define MOON_LED_LEVEL LED_LEVEL
+
+enum custom_keycodes {
+  RGB_SLD = ML_SAFE_RANGE,
+  HSV_0_255_255,
+  HSV_86_255_128,
+  HSV_172_255_255,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_moonlander(
+        KC_DLR, KC_AMPR, KC_LBRACKET, KC_LCBR, KC_LPRN, KC_PLUS, KC_LEFT,
+        KC_RIGHT, KC_ASTR, KC_RPRN, KC_RCBR, KC_RBRACKET, KC_EXLM, KC_HASH,
+        KC_DELETE, KC_QUOTE, KC_COMMA, KC_DOT, KC_P, KC_Y, TG(1), TG(1), KC_F,
+        KC_G, KC_C, KC_R, KC_L, KC_BSLASH, KC_BSPACE, KC_A, KC_O, KC_E, KC_U,
+        KC_I, KC_HYPR, KC_MEH, KC_D, KC_H, KC_T, KC_N, LT(3, KC_S),
+        MT(MOD_LGUI, KC_QUOTE), LM(1, MOD_LSFT), MT(MOD_LCTL, KC_SCOLON), KC_Q,
+        KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, MT(MOD_RCTL, KC_Z), KC_RSHIFT,
+        LT(1, KC_GRAVE), WEBUSB_PAIR, LALT(KC_LSHIFT), KC_LEFT, KC_RIGHT,
+        MT(MOD_LALT, KC_APPLICATION), MT(MOD_LCTL, KC_ESCAPE), KC_UP, KC_DOWN,
+        KC_LBRACKET, KC_RBRACKET, MO(2), KC_SPACE, KC_BSPACE, KC_LGUI, KC_LALT,
+        KC_TAB, KC_ENTER),
+    [1] = LAYOUT_moonlander(
+        KC_TILD, KC_0, KC_1, KC_2, KC_3, KC_4, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_5, KC_6, KC_7, KC_8, KC_9, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT),
+    [2] = LAYOUT_moonlander(
+        KC_ESCAPE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        KC_TRANSPARENT, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_UP, KC_7, KC_8, KC_9, KC_ASTR,
+        KC_F12, KC_TRANSPARENT, KC_HASH, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_DOWN, KC_4, KC_5, KC_6, KC_KP_PLUS,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_PERC, KC_CIRC, KC_LBRACKET,
+        KC_RBRACKET, KC_TILD, KC_AMPR, KC_1, KC_2, KC_3, KC_BSLASH,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_COMMA, HSV_0_255_255, HSV_86_255_128,
+        HSV_172_255_255, RGB_MOD, RGB_TOG, KC_TRANSPARENT, KC_DOT, KC_0,
+        KC_EQUAL, KC_TRANSPARENT, RGB_VAD, RGB_VAI, TOGGLE_LAYER_COLOR, RGB_SLD,
+        RGB_HUD, RGB_HUI),
+    [3] = LAYOUT_moonlander(
+        AU_TOG, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RESET,
+        MU_TOG, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_UP, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, MU_MOD, KC_TRANSPARENT, KC_MS_LEFT, KC_MS_DOWN,
+        KC_MS_RIGHT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,
+        KC_MEDIA_NEXT_TRACK, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN1, KC_MS_BTN2, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN, KC_AUDIO_MUTE,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_WWW_BACK),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case RGB_SLD:
+    if (record->event.pressed) {
+      rgblight_mode(1);
+    }
+    return false;
+  case HSV_0_255_255:
+    if (record->event.pressed) {
+      rgblight_mode(1);
+      rgblight_sethsv(0, 255, 255);
+    }
+    return false;
+  case HSV_86_255_128:
+    if (record->event.pressed) {
+      rgblight_mode(1);
+      rgblight_sethsv(86, 255, 128);
+    }
+    return false;
+  case HSV_172_255_255:
+    if (record->event.pressed) {
+      rgblight_mode(1);
+      rgblight_sethsv(172, 255, 255);
+    }
+    return false;
+  }
+  return true;
+}
